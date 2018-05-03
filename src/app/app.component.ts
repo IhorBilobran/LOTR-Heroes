d3fc34fcc4516f66e04051d553a4125cb730ad7d
ihorPC <ihor.bilobran@gmail.com>
2018-05-03 20:29:48 +0300

    music toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,31 @@ import { HeroListService } from './hero-list.service'
 export class AppComponent {
 	public list;
 	public currentHero;
-	public check: boolean = true
+	public check: boolean = true;
+	public volume: number = 1.0;
 	constructor(private HeroListService: HeroListService){
 		this.list = HeroListService.list;
 	}
 
-	setCurentHero(obj) {
+	setCurentHero(obj: object): any {
 		console.log(obj)
 		this.currentHero = obj;
 		this.check = false;
+	}
+
+	toggleVolume(icon, audio) {
+		switch(icon._elementRef.nativeElement.innerText) {
+			case 'volume_up': this.volume = 0.0; break;
+			case 'volume_off': this.volume = 1.0; break;
+		}
+		return audio.volume = this.volume;
+	}
+
+	get volumeCheck() {
+		if (this.volume < 1) {
+			return 'volume_off';
+		}
+		return 'volume_up';
 	}
 
 }
